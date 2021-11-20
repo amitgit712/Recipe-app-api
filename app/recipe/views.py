f73811dfdcb9c5656d4151a3fd39c0a07b676d00
Amitgit712 <amitgit712@gmail.com>
2021-11-20 13:53:01 +0530

    added create tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,17 +1,25 @@
-from rest_framework import authentication, permissions, \
-                            viewsets, mixins
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from core.models import Tag
 
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """manage tags in the database"""
-    authentication_classes = (authentication.TokenAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new Tag"""
+        serializer.save(user=self.request.user)
